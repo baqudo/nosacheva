419635995ed0fe1e3e9c849705fbbdee8c32722b
BaQudo <baqudo@gmail.com>
2017-02-10 15:55:52 +0200

    add payment and map

--- src/js/app.js
+++ src/js/app.js
@@ -10,5 +10,36 @@ $('.reward-slider').slick({
   arrow: true,
   infinite: true,
   slidesToShow: 4,
-  slidesToScroll: 1
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 2000,
+  responsive: [
+    {
+      breakpoint: 981,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '25%',
+        slidesToShow: 2
+      }
+    },
+    {
+      breakpoint: 701,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '33%',
+        slidesToShow: 1
+      }
+    },
+    {
+      breakpoint: 500,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '25%',
+        slidesToShow: 1
+      }
+    }
+  ]
 });

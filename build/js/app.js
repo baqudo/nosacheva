62bab0c1c9ae9d8133df13598397d88e67a2c56c
BaQudo <baqudo@gmail.com>
2017-02-14 12:59:34 +0200

    link scroll

--- build/js/app.js
+++ build/js/app.js
@@ -2784,3 +2784,15 @@ $('.reward-slider').slick({
     }
   }]
 });
+
+$('.js-payment').on("click", function (event) {
+
+  event.preventDefault();
+  //забираем идентификатор блока с атрибута href
+  var id = $(this).attr('href'),
+
+  //узнаем высоту от начала страницы до блока на который ссылается якорь
+  top = $(id).offset().top - 50;
+  //анимируем переход на расстояние - top за 1600 мс
+  $('body,html').animate({ scrollTop: top }, 1600);
+});
